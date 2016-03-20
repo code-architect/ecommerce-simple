b4129a96e8d034468a2e32bee998cf8012f7a8c3
code-architect <codearchitectdeveloper@gmail.com>
2016-03-20 22:03:55 +0530

    Created a Thank you page

---
+++ public/thankyou.php
@@ -0,0 +1,49 @@
+<?php require_once("../resources/config.php"); ?>
+<?php include(TEMPLATE_FRONT.DS."header.php"); ?>
+<!-- Page Content -->
+
+<?php
+
+if(isset($_GET['tx'])){
+
+
+}
+
+?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Jumbotron Header -->
+        <header class="jumbotron hero-spacer">
+            <h1>A Warm Welcome!</h1>
+            <p>Thank You. For Shopping on our Web Site. To keep on shopping Click on the button below</p>
+            <p><a href="index.php" class="btn btn-primary btn-large">Keep On Shopping!</a>
+            </p>
+        </header>
+
+        <hr>
+        <!-- /Jumbotron Header -->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
