bf5aa5f9527ea0420d9c69f0712088e771ce1fba
code-architect <codearchitectdeveloper@gmail.com>
2016-04-17 20:45:54 +0530

    showing product data in edit_product.php

--- resources/templates/admin_pages/edit_product.php
+++ resources/templates/admin_pages/edit_product.php
@@ -1,54 +1,82 @@
 
-<div class="col-md-12">
+<?php
+// getting the get value
+$id = escape_string($_GET['edit_product']);
 
+// update the products
+update_product($id);
+
+
+
+
+// check if there is a $_GET request and if it is valid
+$value = check_product_exists($id);
+if($value == false){
+    redirect('index.php?products');
+} else {
+
+    $sql = query("SELECT * FROM products WHERE product_id = '{$id}' LIMIT 1");
+    confirm($sql);
+    $row = fetch_array($sql);
+
+?>
+
+
+
+
+<div class="col-md-12">
+    <?php display_message(); ?>
     <div class="row">
         <h1 class="page-header">
             Edit Product
         </h1>
     </div>
 
-    <?php
-   // check if there is a $_GET request and if it is valid
-    $id = check_product_exists(escape_string($_GET['edit_product']));
-    if($id == false){
-        redirect('index.php?products');
-    } else{
-    ?>
 
     <form action="" method="post" enctype="multipart/form-data">
 
 
         <div class="col-md-8">
 
+
+            <!-- Product Title -->
             <div class="form-group">
                 <label for="product-title">Product Title </label>
-                <input type="text" name="product_title" class="form-control">
+                <input type="text" value="<?php echo $row['product_title']; ?>" name="product_title" class="form-control" required="">
 
             </div>
+            <!-- Product Title Ends -->
 
-
+            <!-- Product Description -->
             <div class="form-group">
                 <label for="product-title">Product Description</label>
-                <textarea name="product_description" id="" cols="30" rows="10" class="form-control"></textarea>
+                <textarea name="product_description" id="" cols="30" rows="10" class="form-control" required="">
+                    <?php echo $row['product_description']; ?>
+                </textarea>
             </div>
-
+            <!-- Product Description Ends -->
 
 
             <div class="form-group row">
 
+                <!-- Product Price -->
                 <div class="col-xs-3">
                     <label for="product-price">Product Price</label>
-                    <input type="number" name="product_price" class="form-control" size="60">
+                    <input type="text" value="<?php echo $row['product_price']; ?>" name="product_price" class="form-control" size="60" required="">
                 </div>
+                <!-- Product Price Ends -->
 
             </div>
 
 
-
+            <!-- Product Description Short -->
             <div class="form-group">
                 <label for="product-title">Product Short Description</label>
-                <textarea name="short_desc" id="" cols="30" rows="3" class="form-control"></textarea>
+                <textarea name="product_short_desc" id="" cols="30" rows="3" class="form-control" required="required">
+                    <?php echo $row['product_short_desc']; ?>
+                </textarea>
             </div>
+            <!-- Product Description Short Ends -->
 
 
 
@@ -61,10 +89,9 @@
 
         <aside id="admin_sidebar" class="col-md-4">
 
-
+            <!-- Update Button -->
             <div class="form-group">
-                <input type="submit" name="draft" class="btn btn-warning btn-lg" value="Draft">
-                <input type="submit" name="publish" class="btn btn-primary btn-lg" value="Publish">
+                <input type="submit" name="Update" class="btn btn-primary btn-lg" value="Update">
             </div>
 
 
@@ -74,37 +101,43 @@
                 <label for="product-title">Product Category</label>
 
                 <select name="product_category_id" id="" class="form-control">
-                    <option value="">Select Category</option>
-
-
-
+                    <?php category_for_add_product($row['product_category_id']); ?>
                 </select>
 
+            </div>
+
+
+            <!-- Product Status -->
+            <div class="form-group">
+                <label for="product-title">Product Status</label>
+
+                <select name="product_status" id="product_status" class="form-control">
+                    <option value="1" <?php if($row['product_status'] == 1) {echo "selected";} ?>>On Web</option>
+                    <option value="0" <?php if($row['product_status'] == 0) {echo "selected";} ?>>Drafted</option>
+                </select>
 
             </div>
-            <!-- Product Brands-->
 
 
+            <!-- Product Quantity -->
             <div class="form-group">
                 <label for="product-title">Product Quantity</label>
-                <input type="number" name="product_quantity" class="form-control">
+                <input type="number" value="<?php echo $row['product_quantity']; ?>" name="product_quantity" class="form-control" required="">
             </div>
 
 
-            <!-- Product Tags -->
-
-
-            <!--  <div class="form-group">
-                   <label for="product-title">Product Keywords</label>
-                   <hr>
-                 <input type="text" name="product_tags" class="form-control">
-             </div>
-          -->
-            <!-- Product Image -->
+            <!-- Product Image Small -->
             <div class="form-group">
-                <label for="product-title">Product Image</label>
-                <input type="file" name="file">
+                <label for="product-image">Product Image Small</label>
+                <input type="file" name="image" id="image" ><br>
+                <img src="../../resources/uploads/<?php echo $row['product_image']; ?>" height="75" width="100">
+            </div>
 
+            <!-- Product Image Big -->
+            <div class="form-group">
+                <label for="product-image-big">Product Image Big</label>
+                <input type="file" name="image2" id="image2" ><br>
+                <img src="../../resources/uploads/<?php echo $row['product_image_big']; ?>" height="75" width="100">
             </div>
 
 
@@ -115,4 +148,4 @@
 
     </form>
 
-    <?php } // end of if ?>
+<?php } // end of if ?>
