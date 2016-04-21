965a81427a9568f08b407e32b976d5237074e0de
code-architect <codearchitectdeveloper@gmail.com>
2016-04-21 19:29:17 +0530

    Some experimental sql queries

--- resources/templates/admin_pages/orders.php
+++ resources/templates/admin_pages/orders.php
@@ -40,6 +40,22 @@
 
                 FROM reports a, orders b, products c
                 WHERE a.order_shop_id = b.order_shop_id AND a.product_id = c.product_id
+                _______________________________________________________________________
+                SELECT product_title, product_price from products where product_id
+                IN
+                (select product_id from reports where order_shop_id = '867PZI948Y5O9XG2OY1P7180580')
+                ______________________________________________________________________
+                select a.*, b.* from products a, users b
+                where a.product_id IN (select product_id from reports where order_shop_id = '867PZI948Y5O9XG2OY1P7180580')
+                AND
+                b.user_email = (select user_email from orders where order_shop_id = '867PZI948Y5O9XG2OY1P7180580')
+                ______________________________________________________________________
+                select a.product_title, a.product_price, c.* , b.user_firstname, b.user_lastname, b.user_email
+                  from products a, users b, orders c
+                  where a.product_id IN (select a.product_id from reports where b.order_shop_id = '{$id}')
+                  AND
+                  b.user_email = (select user_email from orders where order_shop_id = '{$id}')
+                  AND c.order_shop_id = '{$id}'
                 -->
             </div>
         </div>
